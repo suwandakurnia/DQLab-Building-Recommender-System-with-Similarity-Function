888e12fc0d8795eb0a7d9e8dc6a224e5487df42f
Suwanda Kurnia <63093353+suwandakurnia@users.noreply.github.com>
2022-04-17 17:06:16 +0700

    Create 01 Import Basics Library and File Unloading.py

---
+++ 01 Import Basics Library and File Unloading.py
@@ -0,0 +1,6 @@
+#import library yang dibutuhkan
+import pandas as pd
+import numpy as np
+
+#lakukan pembacaan dataset
+movie_rating_df = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/movie_rating_df.csv') #untuk menyimpan movie_rating_df.csv
